8f761fb76d16aa5e4e35b84edca68c1a7095723a
unknown <mitsuoysharag@gmail.com>
2019-01-29 23:59:40 -0500

    make it better

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask import jsonify
+from flask_cors import CORS
 from ChatbotResponse import response
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def index():
